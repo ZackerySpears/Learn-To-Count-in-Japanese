87ea35b0adc43a23175a12f2d21aca832574a9e1
zackerymarkusrayspears <your_email@example.com>
2022-01-01 21:44:00 -0600

    Added in 5 Japanese charcters

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+
 function getRandomInt() {
-    var maxNumber = 9999;
-     document.getElementById("numberItSelf").value = Math.floor(Math.random() * maxNumber);
+    var maxNumber = 5; 
+    document.getElementById("numberItSelf").value = Math.floor(Math.random() * maxNumber);
+
+    var japanese = [ '一', '二', '三', '四', '五'];
+    var randomJapaneseNum = japanese[Math.floor(Math.random()*japanese.length)];
+    document.getElementById("japaneseNumberItSelf").value = randomJapaneseNum;
 }
+
+
+
+
