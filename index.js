ebcc87ce0236c816c488c527122dbb3f2477bd2e
zackerymarkusrayspears-git <your_email@example.com>
2022-01-01 23:02:20 -0600

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 
 function getRandomInt() {
-    var maxNumber = 5; 
-    document.getElementById("numberItSelf").value = Math.floor(Math.random() * maxNumber);
+    var englishNumber = ['1','2','3','4','5','6','7','8','9','10']; 
+    var japanese = [ '一', '二', '三', '四', '五','六','七','八','九','十'];
+    var index = Math.floor(Math.random() * englishNumber.length);
+    
 
-    var japanese = [ '一', '二', '三', '四', '五'];
-    var randomJapaneseNum = japanese[Math.floor(Math.random()*japanese.length)];
-    document.getElementById("japaneseNumberItSelf").value = randomJapaneseNum;
+    var randomEnglishNumber = englishNumber[index];
+    var randomJapaneseNumber = japanese[index];
+
+   
+    document.getElementById('numberItSelf').value = randomEnglishNumber;
+    document.getElementById("japaneseNumberItSelf").value = randomJapaneseNumber;
 }
 
+function showEnglishNumber() {
+   
+}
 
 
 
