6825056aa3e75dd4ced26598e198cafa0895fa7e
zackerymarkusrayspears-git <your_email@example.com>
2022-01-09 01:02:32 -0600

    Added new index main page, index.css, and index.js. Changed the original index to practice. Added new feature which hides english number

---
+++ js/index.js
@@ -0,0 +1,24 @@
+
+function getRandomInt() {
+    var englishNumber = ['1','2','3','4','5','6','7','8','9','10']; 
+    var japanese = [ '一', '二', '三', '四', '五','六','七','八','九','十'];
+    var index = Math.floor(Math.random() * englishNumber.length);
+    
+
+    var randomEnglishNumber = englishNumber[index];
+    var randomJapaneseNumber = japanese[index];
+
+    this.randomEnglishNumber = randomEnglishNumber;
+  
+        
+    document.getElementById("japaneseNumberItSelf").value = randomJapaneseNumber;
+}
+
+function englishNumberHidden() {
+    document.getElementById('numberItSelf').value = randomEnglishNumber; 
+    if(randomEnglishNumber.style.display === "block") {
+        randomEnglishNumber.style.display = "block";
+    }else {
+        randomEnglishNumber.style.display = "none";
+    }
+}
--- js/practice.js
+++ js/practice.js
@@ -1,4 +1,3 @@
-
 function getRandomInt() {
     var englishNumber = ['1','2','3','4','5','6','7','8','9','10']; 
     var japanese = [ '一', '二', '三', '四', '五','六','七','八','九','十'];
@@ -8,14 +7,17 @@ function getRandomInt() {
     var randomEnglishNumber = englishNumber[index];
     var randomJapaneseNumber = japanese[index];
 
-   
-    document.getElementById('numberItSelf').value = randomEnglishNumber;
+    this.randomEnglishNumber = randomEnglishNumber;
+  
+        
     document.getElementById("japaneseNumberItSelf").value = randomJapaneseNumber;
 }
 
-function showEnglishNumber() {
-   
+function englishNumberHidden() {
+    document.getElementById('numberItSelf').value = randomEnglishNumber; 
+    if(randomEnglishNumber.style.display === "block") {
+        randomEnglishNumber.style.display = "block";
+    }else {
+        randomEnglishNumber.style.display = "none";
+    }
 }
-
-
-
