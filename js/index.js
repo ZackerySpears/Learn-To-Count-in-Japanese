cdb01d6433ecf2221dbd09e58a719cd24b8278b0
zackerymarkusrayspears-git <your_email@example.com>
2022-01-09 12:09:03 -0600

    Added in quiz li, a cs/js modle block to pop up difficulty level, style all over it

--- js/index.js
+++ js/index.js
@@ -1,24 +1,25 @@
+// Get the modal
+var modal = document.getElementById("myModal");
 
-function getRandomInt() {
-    var englishNumber = ['1','2','3','4','5','6','7','8','9','10']; 
-    var japanese = [ '一', '二', '三', '四', '五','六','七','八','九','十'];
-    var index = Math.floor(Math.random() * englishNumber.length);
-    
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-    var randomEnglishNumber = englishNumber[index];
-    var randomJapaneseNumber = japanese[index];
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-    this.randomEnglishNumber = randomEnglishNumber;
-  
-        
-    document.getElementById("japaneseNumberItSelf").value = randomJapaneseNumber;
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
 }
 
-function englishNumberHidden() {
-    document.getElementById('numberItSelf').value = randomEnglishNumber; 
-    if(randomEnglishNumber.style.display === "block") {
-        randomEnglishNumber.style.display = "block";
-    }else {
-        randomEnglishNumber.style.display = "none";
-    }
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
